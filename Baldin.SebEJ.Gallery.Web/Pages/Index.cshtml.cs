fb1b0859f62e2708bec73a94223d2a69add3693f
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-12-21 17:41:33 +0100

    Add project files.

---
+++ Baldin.SebEJ.Gallery.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baldin.SebEJ.Gallery.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
