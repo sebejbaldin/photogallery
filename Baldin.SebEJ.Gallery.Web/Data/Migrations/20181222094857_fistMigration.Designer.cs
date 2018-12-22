43114349d076c48f1be070a43341661fb38d0900
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-12-22 12:07:19 +0100

    add firstmigration for identity

--- Baldin.SebEJ.Gallery.Web/Data/Migrations/20181222094857_fistMigration.Designer.cs
+++ Baldin.SebEJ.Gallery.Web/Data/Migrations/20181222094857_fistMigration.Designer.cs
@@ -4,14 +4,16 @@ using Baldin.SebEJ.Gallery.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Baldin.SebEJ.Gallery.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181222094857_fistMigration")]
+    partial class fistMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
