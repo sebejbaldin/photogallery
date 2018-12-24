e5916fc6ef5a20ad14fe596f2027c8096676a42a
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-12-24 22:26:47 +0100

    add methods to manipulate data

--- Baldin.SebEJ.Gallery.Data/DataAccess.cs
+++ Baldin.SebEJ.Gallery.Data/DataAccess.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Baldin.SebEJ.Gallery.Data.Models;
+using Dapper;
 using Microsoft.Extensions.Configuration;
 
 namespace Baldin.SebEJ.Gallery.Data
@@ -15,6 +19,127 @@ namespace Baldin.SebEJ.Gallery.Data
         public DataAccess(IConfiguration configuration)
         {
             ConnectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public bool DeletePicture(int Id)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"DELETE FROM [dbo].[Pictures] 
+                               WHERE [Id] = @Id";
+                return conn.Execute(sql, new { Id }) > 0;
+            }
+        }
+
+        public bool DeleteVote(int Id)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"DELETE FROM [dbo].[Votes] 
+                               WHERE [Picture_Id] = @Id";
+                return conn.Execute(sql, new { Id }) > 0;
+            }
+        }
+
+        public Picture GetPicture(int Id)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"SELECT *
+                               FROM [dbo].[Pictures]
+                               WHERE [Id] = @Id";
+                return conn.QueryFirstOrDefault<Picture>(sql, new { Id });
+            }
+        }
+
+        public IEnumerable<Picture> GetPictures()
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"SELECT *
+                               FROM [dbo].[Pictures]";
+                return conn.Query<Picture>(sql);
+            }
+        }
+
+        public IEnumerable<Vote> GetVotes()
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"SELECT *
+                               FROM [dbo].[Votes]";
+                return conn.Query<Vote>(sql);
+            }
+        }
+
+        public IEnumerable<Vote> GetVotesByUserId(string userId)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"SELECT *
+                               FROM [dbo].[Votes]
+                               WHERE [User_Id] = @Id";
+                return conn.Query<Vote>(sql, new { Id = userId });
+            }
+        }
+
+        public bool InsertPicture(Picture picture)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"INSERT INTO [dbo].[Pictures]
+                                    ([Name]
+                                    ,[Url]
+                                    ,[Votes]
+                                    ,[Total_Rating])
+                               VALUES
+                                    (@Name
+                                    ,@Url
+                                    ,@Votes
+                                    ,@Total_Rating)";
+                return conn.Execute(sql, picture) > 0;
+            }
+        }
+
+        public bool InsertVote(Vote vote)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"INSERT INTO [dbo].[Votes]
+                                    ([User_Id]
+                                    ,[Picture_Id]
+                                    ,[Vote])
+                               VALUES
+                                    (@User_Id
+                                    ,@Picture_Id
+                                    ,@Rating)";
+                return conn.Execute(sql, vote) > 0;
+            }
+        }
+
+        public bool UpdatePicture(Picture picture)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"UPDATE [dbo].[Pictures]
+                               SET [Name] = @Name
+                                  ,[Url] = @Url
+                                  ,[Votes] = @Votes
+                                  ,[Total_Rating] = @Total_Rating
+                               WHERE [Id] = @Id";
+                return conn.Execute(sql, picture) > 0;
+            }
+        }
+
+        public bool UpdateVote(Vote vote)
+        {
+            using (var conn = new SqlConnection(ConnectionString))
+            {
+                string sql = @"UPDATE [dbo].[Votes]
+                               SET [Vote] = @Vote
+                               WHERE [User_Id] = @User_Id AND [Picture_Id] = @Picture_Id";
+                return conn.Execute(sql, vote) > 0;
+            }
         }
     }
 }
--- Baldin.SebEJ.Gallery.Data/IDataAccess.cs
+++ Baldin.SebEJ.Gallery.Data/IDataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Baldin.SebEJ.Gallery.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,6 +7,16 @@ namespace Baldin.SebEJ.Gallery.Data
 {
     public interface IDataAccess
     {
+        Picture GetPicture(int Id);
+        IEnumerable<Picture> GetPictures();
+        bool InsertPicture(Picture picture);
+        bool UpdatePicture(Picture picture);
+        bool DeletePicture(int Id);
 
+        IEnumerable<Vote> GetVotes();
+        IEnumerable<Vote> GetVotesByUserId(string userId);
+        bool InsertVote(Vote vote);
+        bool UpdateVote(Vote vote);
+        bool DeleteVote(int Id);
     }
 }
