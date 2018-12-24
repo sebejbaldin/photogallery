5471dea31de0d23858b808bedb1c9204800c6815
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-12-24 14:32:52 +0100

    add models for data

---
+++ Baldin.SebEJ.Gallery.Data/Models/Picture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baldin.SebEJ.Gallery.Data.Models
+{
+    public class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public int Votes { get; set; }
+        public long Total_Rating { get; set; }
+
+        public double Rating {
+            get {
+                if (Votes > 0)
+                    return Total_Rating / Votes;
+                else
+                    return 0;
+            }
+        }
+
+    }
+}
---
+++ Baldin.SebEJ.Gallery.Data/Models/Vote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baldin.SebEJ.Gallery.Data.Models
+{
+    public class Vote
+    {
+        public string User_Id { get; set; }
+        public int Picture_Id { get; set; }
+        public byte Rating { get; set; }
+    }
+}
