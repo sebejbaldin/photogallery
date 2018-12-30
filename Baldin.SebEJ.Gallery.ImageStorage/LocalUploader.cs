7c716ca2aff7e09edd37013ada9640391231b0a4
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2018-12-30 17:50:19 +0100

    remove wrong dependency

--- Baldin.SebEJ.Gallery.ImageStorage/LocalUploader.cs
+++ Baldin.SebEJ.Gallery.ImageStorage/LocalUploader.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.IO;
