7f944411ce6e66a516605ec024fc84fcef1ed3ab
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-01-06 18:25:04 +0100

    add ranking

---
+++ Baldin.SebEJ.Gallery.Web/Pages/Gallery/Ranking.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Baldin.SebEJ.Gallery.Data;
+using Baldin.SebEJ.Gallery.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baldin.SebEJ.Gallery.Web.Pages.Gallery
+{
+    public class RankingModel : PageModel
+    {
+        private IDataAccess _dataAccess;
+
+        public RankingModel(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+
+        public IEnumerable<Picture> Pictures { get; set; }
+
+        public void OnGet()
+        {
+            var list = _dataAccess.GetPictures();
+            var ordinated = _dataAccess.GetPictures().ToList();
+            ordinated.Sort((y, x) =>
+            {
+                if (x.Rating == y.Rating)
+                {
+                    if (x.Votes == y.Votes)
+                        return 0;
+                    else if (x.Votes > y.Votes)
+                        return 1;
+                    else
+                        return -1;
+                }
+                else
+                {
+                    return (x.Rating > y.Rating) ? 1 : -1;
+                }
+            });
+            Pictures = ordinated.AsEnumerable();
+        }
+    }
+}
