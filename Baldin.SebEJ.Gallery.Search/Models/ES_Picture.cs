72b031c88dfed300797657f6ebe4c69b818ac590
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-02-13 21:33:05 +0100

    add models for the data to search

---
+++ Baldin.SebEJ.Gallery.Search/Models/ES_Picture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baldin.SebEJ.Gallery.Search.Models
+{
+    public class ES_Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Votes { get; set; }
+        public double Rating { get; set; }
+        public string Thumbnail_Url { get; set; }
+    }
+}
---
+++ Baldin.SebEJ.Gallery.Search/Models/ES_UserPhotos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Baldin.SebEJ.Gallery.Search.Models
+{
+    public class ES_UserPhotos
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ES_Picture> Pictures { get; set; }
+    }
+}
