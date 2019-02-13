6f1205aed144728137c1afd3c4ade80088d57c37
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-02-13 22:15:39 +0100

    add interface to abstract search

--- Baldin.SebEJ.Gallery.Search/ISearch.cs
+++ Baldin.SebEJ.Gallery.Search/ISearch.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Baldin.SebEJ.Gallery.Search.Models;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Baldin.SebEJ.Gallery.Search
 {
     public interface ISearch
     {
-
+        Task<bool> BulkInsertUsersPicturesAsync(IEnumerable<ES_UserPhotos> usersPhotos);
+        Task<bool> InsertUserPicturesAsync(ES_UserPhotos userPhotos);
+        Task<bool> UpdateUserPicturesAsync(ES_UserPhotos userPhotos);
+        Task<IEnumerable<ES_UserPhotos>> SearchAsync(string query);
+        Task<IEnumerable<ES_Picture>> SearchPhotosAsync(string query);
     }
 }
