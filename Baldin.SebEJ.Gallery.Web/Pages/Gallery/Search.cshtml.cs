cbfc020aab3d3c115ae8ab2f520ac53188501c03
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-02-16 23:33:03 +0100

    add simple page for search

---
+++ Baldin.SebEJ.Gallery.Web/Pages/Gallery/Search.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Baldin.SebEJ.Gallery.Data.Models;
+using Baldin.SebEJ.Gallery.Search;
+using Baldin.SebEJ.Gallery.Search.Models;
+using Baldin.SebEJ.Gallery.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Baldin.SebEJ.Gallery.Web.Pages.Gallery
+{
+    public class SearchModel : PageModel
+    {
+        private ISearch _search;
+        private UserManager<IdentityUser> _userManager;
+
+        public SearchModel(ISearch search, UserManager<IdentityUser> userManager)
+        {
+            _search = search;
+            _userManager = userManager;
+        }
+
+        public IEnumerable<User_Picture> Photos { get; set; }
+
+        public async Task OnGet(string query = null)
+        {
+            var list = new List<User_Picture>();
+            if (query != null)
+            {
+                var res = await _search.SearchAsync(query);
+                foreach (var item in res)
+                {
+                    var userData = await _userManager.FindByEmailAsync(item.Email);
+                    list.AddRange(item.Pictures.Select(e => new User_Picture
+                    {
+                        Id = e.Id,
+                        Rating = e.Rating,
+                        Thumbnail_Url = e.Thumbnail_Url,
+                        Url = e.Thumbnail_Url,
+                        Author = userData.Id,
+                        Votes = e.Votes,
+                        IsVoted = true
+                    }));
+                }
+            }
+            Photos = list;
+        }
+    }
+}
