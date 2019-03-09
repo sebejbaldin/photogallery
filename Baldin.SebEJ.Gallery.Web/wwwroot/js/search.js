386ef592cd7299edd3a7752df9f0512e89053a2d
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-03-09 16:25:15 +0100

    add autosearch while typing

---
+++ Baldin.SebEJ.Gallery.Web/wwwroot/js/search.js
@@ -0,0 +1,18 @@
+﻿
+document.getElementById('searchBox').addEventListener('input', async (event) => {
+    let query = event.target.value;
+    if (query.length > 2) {
+        let data = await fetch(`/api/v1/search/1?query=${query}`, {
+                method: 'GET',
+                credentials: 'include'
+            })
+            .then(json => {
+                if (json.ok)
+                    return json.json();
+            })
+            .then(data => data);
+        if (data) {
+            writeCards(data.photos);
+        }
+    }
+});
