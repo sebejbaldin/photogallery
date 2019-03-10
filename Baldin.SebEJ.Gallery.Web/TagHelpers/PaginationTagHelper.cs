85be920682f02331ff15cd6ae74a4a6758491b4d
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-03-10 02:13:25 +0100

    add support to query strings

--- Baldin.SebEJ.Gallery.Web/TagHelpers/PaginationTagHelper.cs
+++ Baldin.SebEJ.Gallery.Web/TagHelpers/PaginationTagHelper.cs
@@ -13,6 +13,7 @@ namespace Baldin.SebEJ.Gallery.Web.TagHelpers
     {
         public string Url { get; set; }
         public PaginatedList<User_Picture> Pictures { get; set; }
+        public IDictionary<string, string> QueryStrings { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -20,20 +21,33 @@ namespace Baldin.SebEJ.Gallery.Web.TagHelpers
             {
                 Url += "/";
             }
+            string queryes = "";
             StringBuilder builder = new StringBuilder();
+
+            if (QueryStrings != null && QueryStrings.Count > 0)
+            {
+                builder.Append("?");
+                foreach (var item in QueryStrings)
+                {
+                    builder.Append($"{item.Key}={item.Value}&");
+                }
+                builder.Remove(builder.Length - 1, 1);
+                queryes = builder.ToString();
+            }
+            builder.Clear();
             builder.Append("<nav aria-label=\"gallery pagination\">");
             builder.Append("<ul class=\"pagination justify-content-center\">");
             builder.Append($"<li id=\"prevPage\" class=\"page-item {(Pictures.HasPreviousPage ? "" : "disabled")}\">");
-            builder.Append($"<a class=\"page-link\" href=\"{Url}{Pictures.PageIndex - 1}\" aria-label=\"Previous\">");
+            builder.Append($"<a class=\"page-link\" href=\"{Url}{Pictures.PageIndex - 1}{queryes}\" aria-label=\"Previous\">");
             builder.Append("<span aria-hidden=\"true\">&laquo;</span><span class=\"sr-only\">Previous</span></a></li>");
 
             foreach (var index in Pictures.GetRoutePages())
             {
-                builder.Append($"<li class=\"page-item {(Pictures.PageIndex == index ? "active" : "")}\"><a class=\"page-link\" href=\"{Url}{index}\">{index}</a></li>");
+                builder.Append($"<li class=\"page-item {(Pictures.PageIndex == index ? "active" : "")}\"><a class=\"page-link\" href=\"{Url}{index}{queryes}\">{index}</a></li>");
             }
 
             builder.Append($"<li id=\"nexPage\" class=\"page-item {(Pictures.HasNextPage ? "" : "disabled")}\">");
-            builder.Append($"<a class=\"page-link\" href=\"{Url}{Pictures.PageIndex + 1}\" aria-label=\"Next\">");
+            builder.Append($"<a class=\"page-link\" href=\"{Url}{Pictures.PageIndex + 1}{queryes}\" aria-label=\"Next\">");
             builder.Append("<span aria-hidden=\"true\">&raquo;</span>");
             builder.Append("<span class=\"sr-only\">Next</span>");
             builder.Append("</a></li></ul></nav>");
