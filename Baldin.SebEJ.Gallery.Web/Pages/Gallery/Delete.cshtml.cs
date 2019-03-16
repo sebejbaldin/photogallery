930dcd6973e37666eb755ea64aae630dc088f680
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-03-16 20:13:26 +0100

    when deleted the photo data is removed from everywhere

--- Baldin.SebEJ.Gallery.Web/Pages/Gallery/Delete.cshtml.cs
+++ Baldin.SebEJ.Gallery.Web/Pages/Gallery/Delete.cshtml.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Baldin.SebEJ.Gallery.Caching;
 using Baldin.SebEJ.Gallery.Data;
 using Baldin.SebEJ.Gallery.Data.Models;
+using Baldin.SebEJ.Gallery.Search;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -16,11 +18,15 @@ namespace Baldin.SebEJ.Gallery.Web.Pages.Gallery
     public class DeleteModel : PageModel
     {
         private IDataAccess _dataAccess;
+        private ISearch _search;
+        private ICaching _caching;
         private UserManager<IdentityUser> _userManager;
 
-        public DeleteModel(IDataAccess dataAccess, UserManager<IdentityUser> userManager)
+        public DeleteModel(IDataAccess dataAccess, ISearch search, ICaching caching, UserManager<IdentityUser> userManager)
         {
             _dataAccess = dataAccess;
+            _search = search;
+            _caching = caching;
             _userManager = userManager;
         }
 
@@ -36,7 +42,11 @@ namespace Baldin.SebEJ.Gallery.Web.Pages.Gallery
             var pic = await _dataAccess.GetPictureAsync(photoId);
             var userId = User.FindFirst("userId");
             if (userId.Value == pic.User_Id)
+            {
                 await _dataAccess.DeletePictureAsync(photoId);
+                await _search.DeletePhotoAsync(photoId);
+                await _caching.DeletePictureAsync(photoId);
+            }
             return RedirectToPage("/Gallery/Index");
         }
     }
