41290eeb34681689e7335fa5ae2cf6d13c914523
Sebastiano Baldin <baldin.sebastiano@gmail.com>
2019-03-16 14:57:35 +0100

    move class from CommentsV1Controller

---
+++ Baldin.SebEJ.Gallery.Web/Models/ThisShouldNotExist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Baldin.SebEJ.Gallery.Web.Models
+{
+    public class ThisShouldNotExist
+    {
+        public string TextBody { get; set; }
+    }
+}
